d261351eeddefdf764c274cbcb4804eef039ed64
Nathan Sorenson <nathan.d.sorenson@gmail.com>
2015-03-25 23:53:27 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject lein-lock "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A Leiningen plugin to check transitive dependencies against a dependencies.lock file to ensure repeatable builds."
+  :url "sparkfund.co"
   :dependencies [[pandect "0.5.1"]]
   :eval-in-leiningen true)
