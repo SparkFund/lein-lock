933f876eb45f497b8ec9f914e48984252d610c73
Jeffrey Stanton <jffry@users.noreply.github.com>
2015-07-06 13:55:22 -0400

    Adds Apache License, bumps version number to v0.1.1

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
-(defproject lein-lock "0.1.0-SNAPSHOT"
+(defproject lein-lock "0.1.1"
   :description "A Leiningen plugin to check transitive dependencies against a dependencies.lock file to ensure repeatable builds."
-  :url "sparkfund.co"
+  :url "https://github.com/SparkFund/lein-lock"
   :dependencies [[pandect "0.5.1"]]
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :scm {:name "git"
+        :url "https://github.com/SparkFund/lein-lock"}
   :eval-in-leiningen true)
